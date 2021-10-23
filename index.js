51cd5394301b466dcc29a05d8d998f08854a6222
Stuart <86697483+Pilot67@users.noreply.github.com>
2021-10-23 11:50:40 +1100

    added js and css

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const inquirer = require('inquirer');
-const fs = require('fs/promise');
+const fs = require('fs');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 const Manager = require('./lib/Manager');
 const companyEmployee = [];
-let role = "manager"
 
+let role = "manager"
 const rolePrompt = [
     {
         type: "input",
@@ -21,6 +21,16 @@ const rolePrompt = [
         type: "input",
         name: "email",
         message: `Please enter the ${role}'s email address -`,
+        default: `stuart@simmons1.net`,
+        validate: (email) => {
+            valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email);
+            if (valid) {
+              return true;
+            } else {
+              console.log(".  Please enter a valid email");
+              return false;
+            }
+          },
     },  
     {
         type: "input",
@@ -28,3 +38,71 @@ const rolePrompt = [
         message: `Please enter the office phone number`,
     },
 ];
+
+
+const promptEmployee = () => {
+    return inquirer.prompt(rolePrompt)
+    .then(({name, id, email, ...rest}) => {
+        switch (role) {
+            case "manager":
+                let manager = new Manager(name,id,email,rest.officeNumber)
+                companyEmployee.push(manager);
+                break;
+            case "engineer":
+                let engineer = new Engineer(name,id,email,rest.github)
+                companyEmployee.push(engineer);
+                break;
+            case "intern":
+                let intern = new Intern(name,id,email,rest.school)
+                companyEmployee.push(intern);
+        }
+        
+        console.log(companyEmployee)
+        companyEmployee.forEach(element  => {
+            console.log(element.getRole(), " - This Role")
+        });
+
+    })
+    .then(() => {
+        // Add New employee
+            return inquirer.prompt([
+                {
+                    type: "list",
+                    name: "addRole",
+                    message: "Choose the role of the new employee",
+                    choices: ['Engineer','Intern', 'I dont need to add any more']
+                },
+            ])
+            .then(({addRole}) => {
+                if (addRole === "Engineer") {
+                    role = "engineer";
+                    rolePrompt[0].message = `Please enter the ${role}'s name -`,
+                    rolePrompt[1].message = `Please enter the ${role}'s employee's ID -`,
+                    rolePrompt[2].message =  `Please enter the ${role}'s email address -`,
+                    rolePrompt[3].name = "github"
+                    rolePrompt[3].message = "Please enter the engineers's github profile name -"
+                    promptEmployee()
+                } else if (addRole === "Intern") {
+                    role = "intern";
+                    rolePrompt[0].message = `Please enter the ${role}'s name -`,
+                    rolePrompt[1].message = `Please enter the ${role}'s employee's ID -`,
+                    rolePrompt[2].message =  `Please enter the ${role}'s email address -`,
+                    rolePrompt[3].name = "school"
+                    rolePrompt[3].message = "Please enter the school's name -"
+                    promptEmployee()
+                } else {
+                    // End of inputs, call HTML build
+                    throw new Error ("Die")
+                }
+            })
+
+    })
+    .catch((err) => console.log("This error is ", err));
+}
+
+
+
+//Init
+(() => {
+    promptEmployee()
+})();
